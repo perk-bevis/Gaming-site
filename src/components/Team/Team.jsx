025b9458590b807198faf38bac9cdd4e77907e78
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-20 21:25:07 +0700

    Update Team.jsx

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -29,7 +29,7 @@ function Team(){
                 900: {
                     slidesPerView: 2
                 },
-                750: {
+                640: {
                     slidesPerView: 1
                 },
     
