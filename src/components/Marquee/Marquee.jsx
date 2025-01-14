366b506d03a856d695e91a319bd1c8980a55ab51
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-01-14 16:52:00 +0700

    Update Marquee.jsx

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -6,13 +6,13 @@ function Marquee() {
   return (
     <marquee behavior="alternate" direction="">
       <div className={MarqueeCss.marquee_text}>
-        <h1>Gaming Community</h1>
+        <h1>Cộng đồng chơi game</h1>
         <img src={skullImg} alt="" className={MarqueeCss.skullImg} />
-        <h1>Inclusive Gaming</h1>
+        <h1>Chơi game toàn diện</h1>
         <img src={skullImg} alt="" className={MarqueeCss.skullImg} />
-        <h1>Gaming Community</h1>
+        <h1>Cộng đồng chơi game</h1>
         <img src={skullImg} alt="" className={MarqueeCss.skullImg} />
-        <h1>Inclusive Gaming</h1>
+        <h1>Chơi game toàn diện</h1>
         <img src={skullImg} alt="" className={MarqueeCss.skullImg} />
       </div>
     </marquee>
